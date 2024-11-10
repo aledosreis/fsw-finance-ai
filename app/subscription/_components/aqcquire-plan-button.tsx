08866dcb4eab08b8cd348c60673b8a02be3625f9
Alessandro Reis <alessandrodreis23@gmail.com>
2024-11-10 08:08:04 -0300

    feat: add stripe webhook that handle subscriptions

--- app/subscription/_components/aqcquire-plan-button.tsx
+++ app/subscription/_components/aqcquire-plan-button.tsx
@@ -3,8 +3,11 @@
 import { Button } from "@/app/_components/ui/button";
 import { createStripeCheckout } from "../_actions/create-stripe-checkout";
 import { loadStripe } from "@stripe/stripe-js";
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 export function AqcuirePlanButton() {
+  const { user } = useUser();
   async function handleAcquirePlanClick() {
     const { sessionId } = await createStripeCheckout();
 
@@ -24,7 +27,19 @@ export function AqcuirePlanButton() {
       sessionId: sessionId,
     });
   }
+  const hasPremiumPlan = user?.publicMetadata.subscriptionPlan === "premium";
 
+  if (hasPremiumPlan) {
+    return (
+      <Button className="w-full rounded-full font-bold" variant="link">
+        <Link
+          href={`${process.env.NEXT_PUBLIC_STRIPE_CUSTOMER_PORTAL_URL as string}?prefilled_email=${user?.emailAddresses[0].emailAddress}`}
+        >
+          Gerenciar Plano
+        </Link>
+      </Button>
+    );
+  }
   return (
     <Button
       onClick={handleAcquirePlanClick}
--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,9 +1,10 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { Navbar } from "../_components/navbar";
 import { redirect } from "next/navigation";
 import { Card, CardContent, CardHeader } from "../_components/ui/card";
 import { CheckIcon, XIcon } from "lucide-react";
 import { AqcuirePlanButton } from "./_components/aqcquire-plan-button";
+import { Badge } from "../_components/ui/badge";
 
 export default async function SubscriptionPage() {
   const { userId } = await auth();
@@ -11,6 +12,8 @@ export default async function SubscriptionPage() {
   if (!userId) {
     redirect("/login");
   }
+  const user = await clerkClient().users.getUser(userId);
+  const hasPremiumPlan = user.publicMetadata.subscriptionPlan === "premium";
 
   return (
     <>
@@ -44,7 +47,12 @@ export default async function SubscriptionPage() {
           </Card>
 
           <Card className="w-[450px]">
-            <CardHeader className="border-b border-solid py-8">
+            <CardHeader className="relative border-b border-solid py-8">
+              {hasPremiumPlan && (
+                <Badge className="absolute left-4 top-12 bg-primary/10 text-primary">
+                  Ativo
+                </Badge>
+              )}
               <h2 className="text-center text-2xl font-semibold">
                 Plano Premium
               </h2>
